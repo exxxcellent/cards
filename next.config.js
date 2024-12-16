f4ff434fd09ce13ab0a1ac20c3f2a2ac25fdcbea
exxxcellent <aleksejdikun@mail.ru>
2024-12-16 12:09:30 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
         ignoreBuildErrors: true,
     },
     distDir: 'build',
+    output: 'export',
+    basePath: '/cards',
+    assetPrefix: '/cards',
 };
 
 module.exports = nextConfig;
